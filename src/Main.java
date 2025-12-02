33deb265652589d0685508bb2fbe898596e1a696
Moe <mohammed.kaldi@outlook.com>
2025-12-02 22:59:09 +0100

    Lade till Scanner och läste in namnIndex från användaren

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 void main() {
 
     System.out.println("Programmet startar ");
@@ -7,5 +10,12 @@ void main() {
     System.out.println("Index 0: " + names[0]);
     System.out.println("Index 1: " + names[1]);
     System.out.println("Index 2: " + names[2]);
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Skriv vilket index (0-2) du vill se: ");
+    int namnIndex = scan.nextInt();
+
+    System.out.println("Du valde index " + namnIndex + ", namnet är: " + names[namnIndex]);
 
 }
