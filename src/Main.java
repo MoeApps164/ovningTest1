c1beff78a3872cd334503b583f570f6e686d7089
Moe <mohammed.kaldi@outlook.com>
2025-12-02 21:37:09 +0100

    Skrev ut alla namn med index

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,8 @@ void main() {
 
     String[] names = {"adam ", "kaldi", "mohammed"};
 
+    System.out.println("Index 0: " + names[0]);
+    System.out.println("Index 1: " + names[1]);
+    System.out.println("Index 2: " + names[2]);
+
 }
