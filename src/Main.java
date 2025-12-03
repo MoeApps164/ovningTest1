e60803360010edbf3b1e46224c0e4d0dca7113f3
Moe <mohammed.kaldi@outlook.com>
2025-12-03 16:53:13 +0100

    la till whie loop och nextline  så at den inte fastnar i en evig loop vid fel och avslutas när man skriver fel och man måste börja om

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,24 @@ void main() {
     System.out.println("Index 2: " + names[2]);
 
     Scanner scan = new Scanner(System.in);
+    boolean val = false;
 
-    try {
-        System.out.print("Skriv vilket index (0-2) du vill se: ");
-        int index = scan.nextInt();
+while (!val) {
+        try {
+            System.out.print("Skriv vilket index (0-2) du vill se: ");
+            int index = scan.nextInt();
 
-        System.out.println("Du valde index " + index + ", namnet är: " + names[index]);
+            System.out.println("Du valde index " + index + ", namnet är: " + names[index]);
 
-    } catch (InputMismatchException e) {
-        System.out.println("Fel: du måste skriva ett heltal.");
+              val = true;
 
-    } catch (ArrayIndexOutOfBoundsException e) {
-        System.out.println("Fel: index måste vara 0, 1 eller 2.");
+        } catch (InputMismatchException e) {
+            System.out.println("Fel: du måste skriva ett heltal.");
+            scan.nextLine();
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Fel: index måste vara 0, 1 eller 2.");
+            scan.nextLine();
+        }
     }
 }
